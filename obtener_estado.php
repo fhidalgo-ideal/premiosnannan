8ce22b13444ca3ed54399dec699db1f4e3f9883f
fhidalgo-ideal <fhidalgo@ideal.es>
2026-09-23 11:33:03 +0200

    fix:Se arregla error en votación

--- obtener_estado.php
+++ obtener_estado.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: application/json');
 header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
 
 require_once 'db.php';
 
